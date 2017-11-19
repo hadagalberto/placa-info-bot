55531f8011be243c341bda4b83981cfe22157de1
Hadagalberto <hadagalberto@users.noreply.github.com>
2017-11-19 02:15:27 -0200

    first release

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='BotSinesp',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Hadagalberto Júnior',
+    author_email='hadagalberto.junior1@gmail.com',
+    description=''
+)
